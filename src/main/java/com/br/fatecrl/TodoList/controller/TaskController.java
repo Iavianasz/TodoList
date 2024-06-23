fe7e32e97a41a87cbfc52a7c71c42008cb0086d3
Iago Viana <iago63803@gmail.com>
2024-06-22 23:41:56 -0300

    feat: add endpoints for updating, deleting, and partially updating tasks

--- src/main/java/com/br/fatecrl/TodoList/controller/TaskController.java
+++ src/main/java/com/br/fatecrl/TodoList/controller/TaskController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -31,4 +32,26 @@ public class TaskController {
         TaskDTO taskDTO = taskService.getTaskById(id);
         return taskDTO != null ? ResponseEntity.ok(taskDTO) : ResponseEntity.notFound().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskDTO> updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
+        TaskDTO updatedTaskDTO = taskService.updateTask(id, taskDTO);
+        return ResponseEntity.ok(updatedTaskDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskDTO> updateTaskPartial(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
+        TaskDTO updatedTaskDTO = taskService.updateTaskPartial(id, updates);
+        return updatedTaskDTO != null ? ResponseEntity.ok(updatedTaskDTO) : ResponseEntity.notFound().build();
+    }
+
+
+
+
 }
