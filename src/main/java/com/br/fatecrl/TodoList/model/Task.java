e37ff27e3fd3ab42a18df427db28c10f0ce1906e
GuilhermeFrnc <133995611+GuilhermeFrnc@users.noreply.github.com>
2024-06-23 22:55:03 -0300

    Cria a entidade Task com enums e anotações.
    Adiciona o conteúdo da classe Task do pacote model.
    Cria enums para o status e priority.

--- src/main/java/com/br/fatecrl/TodoList/model/Task.java
+++ src/main/java/com/br/fatecrl/TodoList/model/Task.java
@@ -1,4 +1,54 @@
 package com.br.fatecrl.TodoList.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name= "tasks")
+@Data
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Priority priority;
+
+    public enum Status {
+        TODO, IN_PROGRESS, DONE
+    }
+
+    public enum Priority {
+        LOW, MEDIUM, HIGH
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
+
