c80930f4e2e6b30deb522047fe35197b376ec49b
GuilhermeFrnc <133995611+GuilhermeFrnc@users.noreply.github.com>
2024-06-24 01:22:27 -0300

    Adiciona funcionalidade POST.
    TaskController
    	Implementa o endpoint "/api/tasks".
    	Adiciona validação dos campos obrigatórios.
    TaskService
    	Implementa a regra de negócio para criar a tarefa no banco de dados.

--- src/main/java/com/br/fatecrl/TodoList/controller/TaskController.java
+++ src/main/java/com/br/fatecrl/TodoList/controller/TaskController.java
@@ -1,4 +1,21 @@
 package com.br.fatecrl.TodoList.controller;
 
+import com.br.fatecrl.TodoList.dto.TaskDTO;
+import com.br.fatecrl.TodoList.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
+        TaskDTO createdTaskDTO = taskService.createTask(taskDTO);
+        return ResponseEntity.ok(createdTaskDTO);
+    }
 }
--- src/main/java/com/br/fatecrl/TodoList/service/TaskService.java
+++ src/main/java/com/br/fatecrl/TodoList/service/TaskService.java
@@ -1,4 +1,22 @@
 package com.br.fatecrl.TodoList.service;
 
+import com.br.fatecrl.TodoList.dto.TaskDTO;
+import com.br.fatecrl.TodoList.mapper.TaskMapper;
+import com.br.fatecrl.TodoList.model.Task;
+import com.br.fatecrl.TodoList.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public TaskDTO createTask(TaskDTO taskDTO) {
+        Task task = TaskMapper.toEntity(taskDTO);
+        Task savedTask = taskRepository.save(task);
+        return TaskMapper.toDTO(savedTask);
+    }
 }
